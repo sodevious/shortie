e56c5662fccee07e9fef61535e4fb5c8e6c15a3c
Nicole Dominguez <sodevious.net@gmail.com>
2021-08-15 16:05:02 -0400

    refactor drf views based on tutorial

--- entries/views.py
+++ entries/views.py
@@ -1,11 +1,48 @@
-from django.shortcuts import render
+from entries.models import Entry
+from entries.serializers import EntrySerializer
 
-from rest_framework import viewsets
+from django.http import Http404
 
-from .serializers import EntrySerializer
-from .models import Entry
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions, mixins, generics
+from rest_framework.reverse import reverse
+from rest_framework.decorators import api_view
 
 
-class EntryViewSet(viewsets.ModelViewSet):
-    queryset = Entry.objects.all().order_by('entry_title')
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        # 'users': reverse('user-list', request=request, format=format),
+        'entries': reverse('entry-list', request=request, format=format)
+    })
+
+class EntryList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Entry.objects.all()
     serializer_class = EntrySerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    # def post(self, request, *args, **kwargs):
+    #     return self.create(request, *args, **kwargs)
+
+
+class EntryDetail(APIView):
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Entry.objects.get(pk=pk)
+        except Entry.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        entry = self.get_object(pk)
+        serializer = EntrySerializer(entry)
+        return Response(serializer.data)
